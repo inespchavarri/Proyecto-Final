f8ea48b5cd7a16852f1df860c2f9b4cde8c9c091
Ines P. Chavarri <inespchavarri@gmail.com>
2019-10-17 14:53:38 +0200

    creación documento

---
+++ main.py
@@ -0,0 +1,44 @@
+from demo import getCarta, cleanText, cleanTags, tokenWords, remStopWords, wordStem, getCategories
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def main():
+
+    path_mnbc = "modelos/mnbc.pickle"
+    with open(path_mnbc, "rb") as data:
+        mnbc = pickle.load(data)
+
+    tfidf = TfidfVectorizer(ngram_range = (1,2), max_features=100)
+
+    categorias = {
+        "politica" : 1,
+        "otros" : 0
+    }
+
+    texto, etiquetas = getCarta(url)
+
+    # Preparación texto para vectorizar
+
+    texto_limpio = cleanText(texto)
+    texto_procesado = tokenWords(texto_limpio)
+    texto_procesado = remStopWords(texto_procesado)
+    texto_procesado = wordStem(texto_procesado)
+
+    X = [texto]
+    X = tfidf.fit_transform(X).toarray()
+
+    etiquetas = cleanTags(etiquetas)
+    etiquetas = getCategories(etiquetas, categorias)
+
+    y = etiquetas
+
+    y_pred = mnbc.predict(X)
+
+    print("\nArtículo: \n\n{}\n\nPredicción categoria: {} \nCategoría real: {}".format(texto, int(y_pred), y))
+
+
+
+
+if __name__ == '__main__':
+    url = input("Introduce una url: ")
+    main()
